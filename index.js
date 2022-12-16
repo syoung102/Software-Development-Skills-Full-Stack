6e79f8ded2dff9fd5f4dcca876f85a8d1b9e5d2e
seoyoung <yoon3435@inha.edu>
2022-12-16 16:55:29 +0200

    change

--- index.js
+++ index.js
@@ -97,7 +97,7 @@ const server = http.createServer((req, res) => {
 });
 
 // environment variable || 5000 (not found)
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || port;
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 

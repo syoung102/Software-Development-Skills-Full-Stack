23fc330e0d24391872e9917bcc9a8245b43e88c9
seoyoung <yoon3435@inha.edu>
2022-12-23 04:11:04 +0200

    service & http

--- Angular Js Crash/src/app/components/task-item/task-item.component.ts
+++ Angular Js Crash/src/app/components/task-item/task-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Task } from '../../Task';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
@@ -7,7 +7,18 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons';
   templateUrl: './task-item.component.html',
   styleUrls: ['./task-item.component.css']
 })
+
 export class TaskItemComponent {
-  @Input() task: Task
+  @Input() task: Task;
+  @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter();
+  @Output() onToggleReminder: EventEmitter<Task> = new EventEmitter();
   faTimes = faTimes;
+
+  onDelete(task) {
+    this.onDeleteTask.emit(task);
+  }
+
+  onToggle(task) {
+    this.onToggleReminder.emit(task);
+  }
 }
--- Angular Js Crash/src/app/components/tasks/tasks.component.ts
+++ Angular Js Crash/src/app/components/tasks/tasks.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
+import { TaskService } from '../../services/task.service'
 import { Task } from '../../Task';
-import { TASKS } from '../../mock-tasks';
 
 @Component({
   selector: 'app-tasks',
@@ -8,5 +8,21 @@ import { TASKS } from '../../mock-tasks';
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent {
-  tasks: Task[] = TASKS;
+  tasks: Task[] = [];
+
+  // To use a service, you have to add it as a provider into the constructor
+  constructor(private taskService: TaskService) {  }
+
+  ngOnInit(): void {
+   this.taskService.getTasks().subscribe((tasks) => (this.tasks = tasks));
+  }
+
+  deleteTask(task: Task){
+    this.taskService.deleteTask(task).subscribe(() => (this.tasks = this.tasks.filter(t => t.id !== task.id)));
+  }
+
+  toggleReminder(task: Task){
+    task.reminder = !task.reminder;
+    this.taskService.updateTaskReminder(task).subscribe();
+  }
 }

0a4039d250b73310bb9803ae76e26c8e501882f6
seoyoung <yoon3435@inha.edu>
2022-12-24 02:06:10 +0200

    routes

--- Mean-Stack/app.js
+++ Mean-Stack/app.js
@@ -5,20 +5,45 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const passport = require('passport');
 const mongoose = require('mongoose');
+const config = require('./config/database');
+
+// Connect to Database
+mongoose.connect(config.database);
+
+// On Connection
+mongoose.connection.on('connected', () => {
+    console.log('Connected to database '+config.database);
+});
+
+// On Error
+mongoose.connection.on('error', (err) => {
+    console.log('Database error: '+err);
+});
 
 const app = express();
 
+const users = require('./routes/users');
+const { resourceUsage } = require('process');
+
 const port = 3000;
 
-// Cors middle ware - they would get blocked if they try to do certatin requests
+// Cors Middleware - they would get blocked if they try to do certatin requests
 app.use(cors());
 
-// When user enter with get, execute call-back function
+// Set Static folder - place to put client-sid files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Body Parser Middleware - when user submit a form, you can grab the data
+app.use(bodyParser.json());
+
+app.use('/users', users);
+
+// Index Route- When user enter with get, execute call-back function
 app.get('/', (req, res) => {
     res.send('Invalid Endpoint');
 });
 
-// Open server
+// Start Server
 app.listen(port, () => {
     console.log('Server stared on port ' + port);
-})
+});
